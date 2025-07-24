2b9f124e94c800a23312bde6047fbab29e75a219
rishikumar-effi <rishikumar.k@efficienza.in>
2025-07-24 10:46:37 +0530

    Added new svgs for objects

--- src/components/CanvasElements/CanvasElements.tsx
+++ src/components/CanvasElements/CanvasElements.tsx
@@ -1,7 +1,7 @@
 import styles from './CanvasElements.module.css';
 import React from 'react';
 import { ToolBarIcons, Icon } from '../Icons';
-const { DrawCircle, DrawRectangle, Text } = ToolBarIcons;
+const { DrawCircle, DrawRectangle, Text, Group, Image } = ToolBarIcons;
 
 const labelElement = (label: string) => {
     let elementName;
@@ -17,8 +17,17 @@ const labelElement = (label: string) => {
             elementName = "Rectangle";
             break;
         case 'textbox':
+        case 'text':
             element = Text;
             elementName = "Text";
+            break;
+        case 'group':
+            element = Group;
+            elementName = "Group";
+            break;
+        case 'image':
+            element = Image;
+            elementName = "Image";
             break;
         default:
             elementName = label;
@@ -30,24 +39,43 @@ const labelElement = (label: string) => {
 
 const CanvasElements = ({ objects, activeObject, highlightObject }: { objects: any, activeObject: any, highlightObject: any }) => {
     return <div className={styles.component}>
-            <fieldset>
-                <legend>Element(s) in Canvas</legend>
-                {objects.length > 0 ? <ul>
-                    {
-                        objects.map((obj: any) => {
-                            const { element, elementName } = labelElement(obj.type);
+        <fieldset>
+            <legend>Element(s) in Canvas</legend>
+            {objects.length > 0 ? <ul>
+                {
+                    objects.map((obj: any) => {
+                        const { element, elementName } = labelElement(obj.type);
 
-                            return <li key={obj.id} data-focused={activeObject && activeObject.id === obj.id} onClick={() => highlightObject(obj)}>
+                        return <li className={styles.object} key={obj.id} data-focused={activeObject && activeObject.id === obj.id} onClick={() => highlightObject(obj)}>
+                            <div className={styles['object-props']}>
                                 <Icon style={{ width: '1em', height: '1em', color: '#e0e0e0' }}>
                                     {element && React.createElement(element)}
                                 </Icon>
                                 <span>{elementName}</span>
-                            </li>
-                        })
-                    }
-                </ul> : <p className={styles['placeholder-text']}>No objects in canvas</p>}
-            </fieldset>
-        </div>
+                            </div>
+                            {
+                                obj._objects && <ul className={styles.nested}>
+                                    {
+                                        obj._objects.map((nestedObj: any, index: number) => {
+                                            const { element, elementName } = labelElement(nestedObj.type);
+                                            return <li key={index} onClick={() => highlightObject(nestedObj)}>
+                                                <div className={styles['object-props']}>
+                                                    <Icon style={{ width: '1em', height: '1em', color: '#e0e0e0' }}>
+                                                        {element && React.createElement(element)}
+                                                    </Icon>
+                                                    <span>{elementName}</span>
+                                                </div>
+                                            </li>
+                                        })
+                                    }
+                                </ul>
+                            }
+                        </li>
+                    })
+                }
+            </ul> : <p className={styles['placeholder-text']}>No objects in canvas</p>}
+        </fieldset>
+    </div>
 }
 
 export default CanvasElements;
